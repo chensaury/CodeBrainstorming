f4e718d7d97aa2d1da341eaf7844f351f4e64cc3
saury <czh12581@126.com>
2024-03-14 18:31:34 +0800

    //使用带有虚拟头结点的形式重新设计链表

--- README.md
+++ README.md
@@ -1,39 +1,38 @@
 # 代码随想录
 
-#### 介绍
-{**以下是 Gitee 平台说明，您可以替换此简介**
-Gitee 是 OSCHINA 推出的基于 Git 的代码托管平台（同时支持 SVN）。专为开发者提供稳定、高效、安全的云端软件开发协作平台
-无论是个人、团队、或是企业，都能够用 Gitee 实现代码托管、项目管理、协作开发。企业项目请看 [https://gitee.com/enterprises](https://gitee.com/enterprises)}
+# 707.设计链表
 
-#### 软件架构
-软件架构说明
+[力扣题目链接(opens new window)](https://leetcode.cn/problems/design-linked-list/)
+
+题意：
+
+在链表类中实现这些功能：
+
+- get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
+- addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+- addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
+- addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val 的节点。如果 index 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
+- deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
+
+#### ****方法一：单向链表\****
+
+**思路**
+
+实现单向链表，即每个节点仅存储本身的值和后继节点。除此之外，我们还需要一个哨兵（sentinel）节点作为头节点，和一个size参数保存有效节点数。如下图所示。
+![img](file:///C:\Users\ADMINI~1\AppData\Local\Temp\ksohtml35432\wps1.jpg)
+
+初始化时，只需创建头节点 head 和 size即可。实现 get**(**index) 时，先判断有效性，再通过循环来找到对应的节点的值。如下图所示。
+![img](file:///C:\Users\ADMINI~1\AppData\Local\Temp\ksohtml35432\wps2.jpg)
+
+实现addAtIndex(index, val)时，如果index是有效值，则需要找到原来下标为 index的节点的前驱节点 pred，并创建新节点 to_add，将to_add 的后继节点设为 pred 的后继节点，将 pred 的后继节点更新为 to_add，这样就将 to_add 插入到了链表中。最后需要更新 size。这样的操作对于 index=0 也成立，如以下两张图所示。
+![img](file:///C:\Users\ADMINI~1\AppData\Local\Temp\ksohtml35432\wps3.jpg)
+![img](file:///C:\Users\ADMINI~1\AppData\Local\Temp\ksohtml35432\wps4.jpg)
+
+实现 addAtHead(val)和 addAtTail(val) 时，可以借助 addAtIndex(index, val)来实现。
+
+实现 deleteAtIndex(index)，先判断参数有效性。然后找到下标为 index 的节点的前驱节点 pred，通过将 pred 的后继节点更新为 pred 的后继节点的后继节点，来达到删除节点的效果。同时也要更新 size。如下图所示。
+![img](file:///C:\Users\ADMINI~1\AppData\Local\Temp\ksohtml35432\wps5.jpg)
+
+ 
 
 
-#### 安装教程
-
-1.  xxxx
-2.  xxxx
-3.  xxxx
-
-#### 使用说明
-
-1.  xxxx
-2.  xxxx
-3.  xxxx
-
-#### 参与贡献
-
-1.  Fork 本仓库
-2.  新建 Feat_xxx 分支
-3.  提交代码
-4.  新建 Pull Request
-
-
-#### 特技
-
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
-5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
--- src/Untitled-1.c
+++ src/Untitled-1.c
@@ -2,7 +2,7 @@
  * @Author: saury czh12581@126.com
  * @Date: 2024-02-29 09:54:39
  * @LastEditors: saury czh12581@126.com
- * @LastEditTime: 2024-03-13 12:50:59
+ * @LastEditTime: 2024-03-14 18:25:43
  * @FilePath: \d_code\src\Untitled-1.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,64 +10,174 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct LinkedList{
+    int data;
+    struct LinkedList* next;
+} MyLinkedList;
 
-// 定义链表节点结构体
-struct Node {
-    int data;           // 节点数据
-    struct Node* next;  // 指向下一个节点的指针
-};
-
-struct Node *NEWlistnode(int index)
+MyLinkedList* myLinkedListCreate() 
 {
-        struct Node *head=malloc(sizeof(struct Node));
+    MyLinkedList *head=malloc(sizeof(MyLinkedList));
+    head->data=0;
+    head->next=NULL;
+    return head;
+}
+
+int myLinkedListGet(MyLinkedList* obj, int index)
+{
+    if(index<0)
+    {
+        return -1;
+    }
+    for(int i=0; i<index; i++)
+    {
+        if(obj!=NULL)
+        {
+            obj = obj->next;
+        }
+        else
+        {
+            return -1;
+        }   
+    }
+    if(obj!=NULL)
+    {
+        return obj->data;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+//带有虚拟头结点头插法
+void myLinkedListAddAtHead(MyLinkedList* obj, int val)
+{
+    MyLinkedList *newhead = myLinkedListCreate();
+    newhead->data=val;
+    
+    newhead->next=obj->next;//新节点指向首元
+    obj->next=newhead;//虚拟头结点指向新节点
+    return;
+}
+
+//带有虚拟头结点尾插法
+void myLinkedListAddAtTail(MyLinkedList* obj, int val) 
+{
+    MyLinkedList* temp = obj;
+    MyLinkedList* newnode = myLinkedListCreate();
+    newnode->data= val;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=newnode;
+   // free(temp);
+    return ;
+}
+
+void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) 
+{
+    if(obj == NULL || index < 0) return; // 如果链表为空或索引无效，直接返回
+
+    MyLinkedList* new = myLinkedListCreate();
+    new->data = val;
+    
+    MyLinkedList* temp = obj;
+    
+    // 如果插入的位置是头节点
+    if(index == 0) {
+        new->next = temp;
+        obj = new;
+        return;
+    }
+    
+    MyLinkedList* pre = NULL;
+    for(int i = 0; temp != NULL && i < index; i++) {
+        pre = temp;
+        temp = temp->next;
+    }
+    
+    if(temp == NULL) return; // 超出链表长度
+    
+    pre->next = new; // 将前一个节点的 next 指向新节点
+    new->next = temp; // 将新节点的 next 指向原来的节点
+}
+
+
+// void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) 
+// {
+//     MyLinkedList* new = malloc(sizeof(MyLinkedList));
+//     new->data = val;
+//     if(index < 0 || (obj) == NULL) return;
+//     // 如果插入的值位于队首
+//     if(index == 0) {
+//         new->next = (obj->next);
+//         (obj->next) = new;
+//         return;
+//     }
+//     // // 如果插入的值不位于队首
+//     // MyLinkedList* temp = obj;
+//     // MyLinkedList* pre = obj;  
+//     // for(int i = 0; i < index; i++) {
+//     //     pre = temp;
+//     //     temp = temp->next;   
+//     //     if(temp == NULL) {
+//     //         break; // 防止访问空指针
+//     //     }
+//     // } 
+//     // pre->next = new;
+//     // new->next = temp;
+// }
+
+void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) 
+{
+    MyLinkedList *temp = obj;
+    // 如果要删除的是头结点
+    while(obj!=NULL&&obj->data==index)
+    {
+        obj=temp->next;
+        free(temp);
+    }
+    // 如果要删除的不是头结点
+    MyLinkedList *pre = NULL;
+    while(temp!=NULL&&temp->data!=index)  //循环赋值查找，找到后退出循环
+    {
+        pre=temp; //存储上一次的值
+        temp=temp->next; //赋新值，如果满足条件，退出
+    }
+    pre->next=temp->next;//删除节点
+    free(temp);
+// 如果要删除的节点不存在
+    if(temp==NULL)
+    {
+        return;
+    }
+}
+
+void myLinkedListFree(MyLinkedList* obj) {}
+
+
+
+
+
+
+
+
+
+MyLinkedList *NEWlistnode(int index)
+{
+        MyLinkedList *head=malloc(sizeof(MyLinkedList));
         head->data=index;
         head->next=NULL;
 
         return head;
 }
 
-// int creat_node()
-// {
-//     int arr[5]={1,2,3,4,5,};
-//     // struct Node *head =malloc(sizeof(struct Node));
-//     struct Node *head =NEWlistnode(0);
-//     for(int i=0; i<5; i++)
-//     {
-//         struct Node *p=malloc(sizeof(struct Node));
-//         p->next=head;
-//         p->data=arr[i];
-//         head = p;
-//     }
-//     return 0;
-// }
 
-int creat_new_node(struct Node **head,int data)
+void delate_node(MyLinkedList *head,int data)
 {
-    // struct Node *head =malloc(sizeof(struct Node));
-    struct Node *newnode =NEWlistnode(data);  //新建一个节点
-    if(*head==NULL)        //如果传进来的节点是空的，把新建的赋值给它，在第一次有效
-    {
-        *head = newnode;
-        return 0;
-    }
-    // struct Node *creat=*head; //创建一个中间缓冲区
-    // while(creat->next !=NULL)  //判断指向下一个的next是否为空
-    // {
-    //     creat=creat->next;  //循环赋值直到creat为最新的节点
-    // }
-    // creat->next=newnode; //指向新创建的节点
-    // return 0;
-
-    // struct Node *p= malloc(sizeof(struct Node));
-    // p->next=
-    newnode->next=(*head);
-    (*head)= newnode;
-
-}
-
-void delate_node(struct Node *head,int data)
-{
-    struct Node *temp = head;
+    MyLinkedList *temp = head;
     // 如果要删除的是头结点
     while(head!=NULL&&head->data==data)
     {
@@ -75,7 +185,7 @@ void delate_node(struct Node *head,int data)
         free(temp);
     }
     // 如果要删除的不是头结点
-    struct Node *pre;
+    MyLinkedList *pre;
     while(temp!=NULL&&temp->data!=data)  //循环赋值查找，找到后退出循环
     {
         pre=temp; //存储上一次的值
@@ -89,7 +199,7 @@ void delate_node(struct Node *head,int data)
         return;
     }
 
-    //return head;
+    // return head;
 }
 
 
@@ -97,263 +207,21 @@ void delate_node(struct Node *head,int data)
 
 
 int main(){
-    struct Node *head =NULL;
-    creat_new_node(&head,1);
-    creat_new_node(&head,2);
-    creat_new_node(&head,3);
+    MyLinkedList *list =(MyLinkedList *)malloc(sizeof(MyLinkedList));
+    list->next=NULL;
+    myLinkedListAddAtHead(list,1);
+    myLinkedListAddAtHead(list,2);
+    myLinkedListAddAtTail(list,0);
+
     // delate_node(head,3);
-    
-    delate_node(head,2);
-    creat_new_node(&head,4);
+    // MyLinkedList *nhead= malloc(sizeof(MyLinkedList));
+    // nhead->next=head;
+    // myLinkedListAddAtIndex(head,0,1);
+     //delate_node(head,4);
+    // creat_new_node(&head,4);
 }
 
 
 
-
-
-// typedef struct Node1
-// {
-//     int data1;
-//     struct Node1* next1;
-// }NODE1;
-//     NODE1 U;
-//     U.data1 =0;
-
-// struct Node2
-// {
-//     int data2;
-//     struct Node2* next2;
-// };
-//     struct Node2 u2;
-//     u2.data2 =0;
-
-//     typedef struct Node2 NODE2;
-//     NODE2 u3;
-//     u3.data2 =0;
-
-
-// typedef struct ListNodeT
-// {
-//     int val;
-//     struct ListNodeT *next;
-// }ListNode; 
-
-
-
-// // 创建一个新节点
-// struct Node* createNode(int data) {
-//     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-//     if (newNode == NULL) {
-//         printf("内存分配失败\n");
-//         exit(1);
-//     }
-//     newNode->data = data;
-//     newNode->next = NULL;
-//     return newNode;
-// }
-
-// // 在链表末尾插入节点
-// void append(struct Node** head, int data) {
-//     struct Node* newNode = createNode(data);
-//     if (*head == NULL) {
-//         *head = newNode;
-//         return;
-//     }
-//     struct Node* current = *head;
-//     while (current->next != NULL) {
-//         current = current->next;
-//     }
-//     current->next = newNode;
-// }
-
-// // 删除指定值的节点
-// void deleteNode(struct Node** head, int key) {
-//     struct Node* temp = *head, *prev;
-
-//     // 如果要删除的节点是头节点
-//     if (temp != NULL && temp->data == key) {
-//         *head = temp->next;
-//         free(temp);
-//         return;
-//     }
-
-//     // 找到要删除节点的前一个节点
-//     while (temp != NULL && temp->data != key) {
-//         prev = temp;
-//         temp = temp->next;
-//     }
-
-//     // 如果找不到要删除的节点
-//     if (temp == NULL) {
-//         printf("未找到要删除的节点\n");
-//         return;
-//     }
-
-//     // 删除节点
-//     prev->next = temp->next;
-//     free(temp);
-// }
-
-// // 打印链表中的所有节点
-// void printList(struct Node* head) {
-//     while (head != NULL) {
-//         printf("%d ", head->data);
-//         head = head->next;
-//     }
-//     printf("\n");
-// }
-
-// int main() {
-//     // 初始化头节点
-//     struct Node* head = NULL;
-
-//     // 在链表末尾插入节点
-//     append(&head, 1);
-//     append(&head, 2);
-//     append(&head, 3);
-//     append(&head, 4);
-
-//     // 打印链表
-//     printf("链表内容: ");
-//     printList(head);
-
-//     // 删除节点
-//     deleteNode(&head, 3);
-
-//     // 再次打印链表
-//     printf("删除节点后的链表内容: ");
-//     printList(head);
-
-//     return 0;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// enum color
-// {
-//     red,
-//     green = 3,
-//     blue
-// }Color;
-// #define max 100
-
-// int sum(int x, int y);
-
-
-// //c++ standard 链表定义
-// // struct ListNode
-// // {
-// //     int val;
-// //     ListNode *next;
-// //     ListNode(int x) : val(x), next(NULL);{}
-// // };
-
-// typedef struct ListNodeT
-// {
-//     int val;
-//     struct ListNodeT *next;
-// }ListNode; 
-
-
-// int main()
-// {
-
-//     int i = 12;
-//     int *a;
-//     int** b;
-//     a=&i;
-    
-
-
-
-
-
-
-
-//     // int arr[4][10]=
-//     // {
-//     //     { , , , ,*,*, , , , },
-//     //     {*,*,*,*,*,*,*,*,*,*},
-//     //     { , , ,*, , ,*, , , },
-//     //     { , , ,*, , ,*, , , },
-//     // };
-//     // for(int i=0; i<4;i++) 
-//     // {
-//     //     for(int j=0; j<10;j++)
-//     //     {
-//     //         printf("%d ",arr[i][j]);
-//     //     }
-//     //     printf("\n");
-//     // }
-
-//     // printf("     **     \n");
-//     // printf("************\n");
-//     // printf("   *    *   \n");
-//     // printf("   *    *   \n");
-
-
-//     // Color = red;
-
-//     // const int n = 0;
-//     // int arr[9]={0};
-//     // int a = max;
-
-//     // printf("%d\n",red);
-//     // printf("%d\n",green);
-//     // printf("%d\n",blue);
-
-//     // char arr1[] = "abcde";
-//     // char arr2[] = {'a', 'b', 'c', 'd', 'e','\0'};
-//     // printf("%s\n",arr1);
-//     // printf("%s\n",arr2);
-//     // printf("%d\n",strlen("c:\tabc\678\tabc.a"));
-
-
-//     // int num1 = 0;
-//     // int num2 = 0;
-//     // scanf("%d,%d",&num1,&num2);
-//     // // int sum = 0;
-//     // // sum = num1 + num2;
-    
-//     // printf("sum =%d\n",sum(num1, num2));
-    
-//     // printf("%zu\n",sizeof(char));
-//     // printf("%zu\n",sizeof(short));
-//     // printf("%zu\n",sizeof(int));
-//     // printf("%zu\n",sizeof(long));
-//     // printf("%zu\n",sizeof(long long));
-//     // printf("%zu\n",sizeof(float));
-
-//     return 0;
-
-// }
-
-// // int sum(int x, int y)
-// // {
-// //     int a = x + y;
-// //     return a;
-// // }
 
 
